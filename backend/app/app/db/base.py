c3dea6497ecc3610f486d8d04045db6e27e74ae3
hieple-ta <hiep.le@totallyawesome.tv>
2023-03-10 16:24:04 +0700

    Add Car model

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -3,4 +3,5 @@
 from app.db.base_class import Base  # noqa
 from app.models.user import User  # noqa
 from app.models.car_brand import CarBrand  # noqa
+from app.models.car import Car # noqa
 
---
+++ backend/app/app/models/car.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+class Car(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(256), nullable=False)
+    logo_url = Column(String(256), nullable=True)
+    desciption = Column(String(256), nullable=True)
+    status = Column(Integer, default=1)
+    car_brand_id = Column(Integer, ForeignKey("carbrand.id"), nullable=True)
